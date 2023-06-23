1939ff5750b4da48c424f45eb83d7ba5f921faba
tranmanhducslt <79132392+tranmanhducslt@users.noreply.github.com>
2023-06-23 14:18:21 +0700

    be back soon...

--- mqtt.py
+++ mqtt.py
@@ -1,20 +1,2 @@
 import sys
 from Adafruit_IO import MQTTClient
-
-
-AIO_FEED_ID = ""
-AIO_USERNAME = ""
-AIO_KEY = ""
-def connected(client):
-    print("Connected to server!!!")
-    client.subscribe(AIO_FEED_ID)
-
-def subscribe(client , userdata , mid , granted_qos):
-    print("Subscribed successfully...")
-
-def disconnected(client):
-    print("Disconnected from server!")
-    sys.exit (1)
-
-def message(client , feed_id , payload):
-    print("Received data: " + payload)
