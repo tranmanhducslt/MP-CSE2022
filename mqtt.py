6272aeddcf32e817614a926c941c879c4b2e528f
tranmanhducslt <79132392+tranmanhducslt@users.noreply.github.com>
2023-06-30 13:59:26 +0700

    adding code

--- mqtt.py
+++ mqtt.py
@@ -1,10 +1,20 @@
 import sys
 import time
 import random
+import requests
 from Adafruit_IO import MQTTClient
-AIO_USERNAME = "Who_cares"
-AIO_KEY = ""
-global_equation = "x1 + 2 * x2 + 3 * x3"
+AIO_USERNAME = "tranmanhducslt"
+AIO_KEY = "aio_DsIK68UqR6ygoB76EL6IKsObQzxn"
+global_equation = "x1 + x2 + x3"
+
+def init_global_equation():
+    global global_equation
+    headers = {}
+    aio_url = "https://io.adafruit.com/api/v2/tranmanhducslt/feeds/equation"
+    x = requests.get(url=aio_url, headers=headers, verify=False)
+    data = x.json()
+    global_equation = data["last_value"]
+    print("Get latest value:", global_equation)
 
 def connected(client):
     print("Server connected ...")
@@ -26,6 +36,11 @@ def message(client , feed_id , payload):
         global_equation = payload
         print(global_equation)
 
+def modify(x1, x2, x3):
+    global global_equation
+    result = eval(global_equation)
+    return result
+
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 
 client.on_connect = connected # function pointer/callback
@@ -36,10 +51,12 @@ client.on_subscribe = subscribe
 
 client.connect()
 client.loop_background()
+init_global_equation()
 
 while True:
     time.sleep(10) # updating every interval
     client.publish("sensor1", random.randint(25, 40))
     client.publish("sensor2", random.randint(60, 80))
     client.publish("sensor3", random.randint(60, 80)/10)
+    print(modify(random.randint(1,10), random.randint(1,10), random.randint(1,10)))
     pass
