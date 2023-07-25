f60adb869b36966c23b6b44105c87cd86323184c
tranmanhducslt <79132392+tranmanhducslt@users.noreply.github.com>
2023-07-25 20:09:55 +0700

    new AI

--- AI.py
+++ AI.py
@@ -1,6 +1,8 @@
 from keras.models import load_model  # TensorFlow is required for Keras to work
 import cv2  # Install opencv-python
 import numpy as np
+import time
+import sys
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
@@ -12,17 +14,22 @@ model = load_model("keras_Model.h5", compile=False)
 class_names = open("labels.txt", "r").readlines()
 
 # CAMERA can be 0 or 1 based on default camera of your computer
-camera = cv2.VideoCapture(0)
+camera = None 
 
-while True:
+def farm_detect():  
+    global camera
+    camera = cv2.VideoCapture(0)
+
     # Grab the webcamera's image.
     ret, image = camera.read()
+    if image is None: 
+       return 1
 
     # Resize the raw image into (224-height,224-width) pixels
     image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
     # Show the image in a window
-    cv2.imshow("Webcam Image", image)
+    # cv2.imshow("Webcam Image", image)
 
     # Make the image a numpy array and reshape it to the models input shape.
     image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
@@ -39,13 +46,22 @@ while True:
     # Print prediction and confidence score
     print("Class:", class_name[2:], end="")
     print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+    if (class_name[0] == '0' and confidence_score >= .9): return 1
 
     # Listen to the keyboard for presses.
     keyboard_input = cv2.waitKey(1)
 
     # 27 is the ASCII for the esc key on your keyboard.
     if keyboard_input == 27:
-        break
+        quit()
 
-camera.release()
-cv2.destroyAllWindows()
+def startAI():   
+    while True:
+        if farm_detect():
+            camera.release()
+            break
+        else: 
+            time.sleep(3)
+            if not farm_detect():
+                return("No farmer!")
+        time.sleep(1/30)
