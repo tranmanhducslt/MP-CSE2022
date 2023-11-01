831834b2bb8f969cf87e4445dcf7a7debde60757
tranmanhducslt <79132392+tranmanhducslt@users.noreply.github.com>
2023-11-01 23:11:21 +0700

    adding stuff

--- GPT_oop.py
+++ GPT_oop.py
@@ -6,30 +6,29 @@ class GPT: # still trying to make it
     def __init__(self):
         self.user_input = None
         self.role = "User"
-        self.prompt = None
-        self.conversation = None
-    def generate_response(user_input, role="user"):
-        message_history.append({'role': 'system', 'content': 'You are a helpful assistant.'})
-        message_history.append({"role": self.role, "content": f"{self.user_input}"})
+        self.message_history = {}
+    def generate_response(self, role="user"):
+        if len(self.message_history) >= 5:
+            self.message_history.pop(0)
+        self.user_input = input('User:')
+        self.message_history.append({'role': 'system', 'content': 'You are a helpful assistant.'})
+        self.message_history.append({"role": self.role, "content": f"{self.user_input}"})
 
         array_exit = ["", "Bye ChatGPT", "Bye ChatGPT", "bye", "bye chat", "bye", "see you"]
-        if self.user_input in array_exit:
+        if self.user_input in array_exit or not self.user_input:
             return None
         
         completion = openai.ChatCompletion.create(
             model=model_id,
-            messages=message_history
+            messages=self.message_history
         )
-        response = completion.choices[0].message.content
-        print(completion.choices[0].message.content.strip())
-        message_history.append({"role": "assistant", "content": f"{response}"})
-        return response
-
-    message_history = []
+        response = completion.choices[0].message['content'].strip()
+        print(response)
+        self.message_history.append({"role": "assistant", "content": f"{response}"})
+        return response    
 
     while True:
         gpt = GPT()
-        gpt.prompt = input('User:')
-        gpt.conversation = generate_response(gpt.prompt, role="user")
+        gpt.conversation = generate_response(gpt.generate_response, role="user")
         if gpt.conversation is None:
             break
